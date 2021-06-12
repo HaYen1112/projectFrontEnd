068422e8a58a0f0f11eecd823ea0b4ace6dcbc55
hayen1112 <18130295@st.hcmuaf.edu.vn>
2021-06-12 08:57:52 +0700

    thêm node_module lại

--- app-news/src/app/app.module.ts
+++ app-news/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/header/menu/menu.component';
+import { MenuBottomComponent } from './components/header/menu-bottom/menu-bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MenuBottomComponent
   ],
   imports: [
     BrowserModule,
