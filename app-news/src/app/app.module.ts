44df37f322dba0f281488e5d6add06f75ff8e2b7
buianhtuandtc <74261810+buianhtuandtc@users.noreply.github.com>
2021-06-14 15:07:50 +0700

    Hoàn thành menu bottom

--- app-news/src/app/app.module.ts
+++ app-news/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/header/menu/menu.component';
+import { MenuBottomComponent } from './components/header/menu-bottom/menu-bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MenuBottomComponent
   ],
   imports: [
     BrowserModule
