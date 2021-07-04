8292dd5085250d3b18d48ec7293b0b8b69c9d0b6
truongvanxinh <18130292@st.hcmuaf.edu.vn>
2021-07-04 19:56:07 +0700

    Sửa rss xong

--- app-news/src/app/components/body/page-body/body-left/body-left.component.ts
+++ app-news/src/app/components/body/page-body/body-left/body-left.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-body-left',
@@ -6,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body-left.component.scss']
 })
 export class BodyLeftComponent implements OnInit {
+  @Input() dataTitle: any[];
+  @Input() dataImg: any[];
+  @Input() dataDate: any[];
 
-  constructor() { }
+  index = 0;
+  private data: any[];
+  private datas: any[];
+  private url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fcong-nghe%2Fsan-pham-moi.rss';
 
+    constructor(private component: AppComponent) {
+      this.data=[];
+      this.dataDate = [];
+      this.dataImg = [];
+      this.dataTitle = [];
+      this.datas=[];
+      component.getData(this.url)
+      .subscribe(value => {
+        for (const [k, v] of Object.entries(value)) {
+          this.index++;
+          if (this.index == 3){
+            this.data.push(v);
+            for (const [s, x] of Object.entries(this.data[0]))
+                  this.datas.push(x);
+              for (const [s, x] of Object.entries(this.datas)){
+                    this.dataTitle.push(x['title']);
+                    this.dataDate.push(x['pubDate']);
+                    this.dataImg.push(x['thumbnail']);
+              }
+            }
+           }
+      });
+      console.log(this.dataDate);
+      console.log(this.dataTitle);
+      // console.log(this.data.length);
+     }
   ngOnInit(): void {
   }
 
