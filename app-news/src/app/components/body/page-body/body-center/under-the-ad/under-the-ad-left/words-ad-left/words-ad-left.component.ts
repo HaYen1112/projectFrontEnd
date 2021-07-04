c857b11699cd769db2fc5a3d6f2bd488a3e5a6be
buianhtuandtc <74261810+buianhtuandtc@users.noreply.github.com>
2021-07-04 20:31:16 +0700

    Thêm rss cho slide new trang chủ

--- app-news/src/app/components/body/page-body/body-center/under-the-ad/under-the-ad-left/words-ad-left/words-ad-left.component.ts
+++ app-news/src/app/components/body/page-body/body-center/under-the-ad/under-the-ad-left/words-ad-left/words-ad-left.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 @Component({
   selector: 'app-words-ad-left',
   templateUrl: './words-ad-left.component.html',
   styleUrls: ['./words-ad-left.component.scss']
 })
 export class WordsAdLeftComponent implements OnInit {
-  constructor() { }
-
+  @Input() dataTitle: any[];
+  @Input() dataImg: any[];
+  @Input() dataDate: any[];
+   private url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Fvideo%2Fthoi-su.rss';
+  
+    constructor(private component: AppComponent) {
+        component.setMain(this.url);
+        this.dataTitle = component.getDataTitle();
+        this.dataImg = component.getDataImg();
+        this.dataDate = component.getDataDate();
+     }
   ngOnInit(): void {
   }
 
