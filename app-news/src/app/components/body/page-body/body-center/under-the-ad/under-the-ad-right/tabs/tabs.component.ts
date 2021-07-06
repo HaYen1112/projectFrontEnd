af32190f5d9471f565d3cecca45bf870aff4b4ca
hayen1112 <18130295@st.hcmuaf.edu.vn>
2021-07-06 13:08:16 +0700

    Hoàn thành đỗ dữ liệu phần tin gần đây nhất

--- app-news/src/app/components/body/page-body/body-center/under-the-ad/under-the-ad-right/tabs/tabs.component.ts
+++ app-news/src/app/components/body/page-body/body-center/under-the-ad/under-the-ad-right/tabs/tabs.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,Input, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { UnderTheAdRightComponent } from '../under-the-ad-right.component';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent implements OnInit {
+  @Input() dataMostRecent : any[];
 
-  constructor() { }
+  private urlMostRecent =' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fthoi-su%2Fdan-sinh.rss'
+
+  constructor(private underRight: UnderTheAdRightComponent) {
+    this.dataMostRecent =[];
+    this.underRight.setDatas(this.urlMostRecent, 0 , [], [], this.dataMostRecent);
+   }
+   getDataMostRecent(): any[]{
+    return this.dataMostRecent;
+  }
 
   ngOnInit(): void {
   }
