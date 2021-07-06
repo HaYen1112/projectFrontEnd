b670e5b1dabbce7d1728991a0aad824e67bea75a
hayen1112 <18130295@st.hcmuaf.edu.vn>
2021-07-06 13:52:44 +0700

    Hoàn thành đổ dữ liệu Tin Công Nghệ

--- app-news/src/app/components/body/page-body/body-center/under-the-ad/under-the-ad-right/tabs/tabs.component.ts
+++ app-news/src/app/components/body/page-body/body-center/under-the-ad/under-the-ad-right/tabs/tabs.component.ts
@@ -8,15 +8,31 @@ import { UnderTheAdRightComponent } from '../under-the-ad-right.component';
 })
 export class TabsComponent implements OnInit {
   @Input() dataMostRecent : any[];
+  @Input() dataQP : any[];
+  @Input() dataCN : any[];
 
+  // thời sự
   private urlMostRecent =' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fthoi-su%2Fdan-sinh.rss'
-
+  //Quốc phòng
+  private urlOP ='https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fthoi-su%2Fquoc-phong.rss';
+ //Công nghệ
+  private urlCN =' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fcong-nghe%2Fchuyen-doi-so.rss';
   constructor(private underRight: UnderTheAdRightComponent) {
     this.dataMostRecent =[];
+    this.dataQP = [];
+    this.dataCN = [];
     this.underRight.setDatas(this.urlMostRecent, 0 , [], [], this.dataMostRecent);
+    this.underRight.setDatas(this.urlOP, 0,[],[], this.dataQP);
+    this.underRight.setDatas(this.urlCN, 0,[],[], this.dataCN);
    }
    getDataMostRecent(): any[]{
     return this.dataMostRecent;
+  }
+  getDataQP(): any[]{
+    return this.dataQP;
+  }
+  getDataCN(): any[]{
+    return this.dataCN;
   }
 
   ngOnInit(): void {
