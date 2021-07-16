c62ad95a4645968fe9685bc018ad96e8548fd118
buianhtuandtc <74261810+buianhtuandtc@users.noreply.github.com>
2021-07-17 02:34:11 +0700

    Update code và đổ dữ liệu trang Bạn cần biết

--- app-news/src/app/components/you-need-know/under-left/cate-list/cate-list.component.ts
+++ app-news/src/app/components/you-need-know/under-left/cate-list/cate-list.component.ts
@@ -1,13 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { BodyCenterComponent } from 'src/app/components/body/page-body/body-center/body-center.component';
 @Component({
   selector: 'app-cate-list',
   templateUrl: './cate-list.component.html',
   styleUrls: ['./cate-list.component.scss']
 })
 export class CateListComponent implements OnInit {
+ private urlTD ="https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fban-can-biet%2Ftuyen-dung.rss";
+ private urlSP=" https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fban-can-biet%2Fsan-pham.rss";
+ private urlGT=" https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fban-can-biet%2Fgiai-tri.rss";
+ private urlDV="  https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fban-can-biet%2Fdich-vu.rss";
+ private urlGTh=" https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fban-can-biet%2Fgiai-thuong.rss";
+ private urlTB=" https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fban-can-biet%2Fthong-bao.rss"; 
+ @Input() dataTD: any[];
+ @Input() dataSP: any[];
+ @Input() dataGT: any[];
+ @Input() dataDV: any[];
+ @Input() dataGTh: any[];
+ @Input() dataTB: any[];
+ @Input() dataCM: any[]=[{title: ['TUYỂN DỤNG','SẢN PHẨM','GIẢI TRÍ','DỊCH VỤ','GIẢI THƯỞNG','THÔNG BÁO']}];
+ constructor(private cate : BodyCenterComponent) {
+   this.dataTD = [];
+   this.dataSP = [];
+   this.dataGT = [];
+   this.dataDV = [];
+   this.dataGTh = [];
+   this.dataTB = [];
 
-  constructor() { }
+   this.cate.getDatas(this.urlTD,this.dataTD);
+   this.cate.getDatas(this.urlSP,this.dataSP);
+   this.cate.getDatas(this.urlGT,this.dataGT);
+   this.cate.getDatas(this.urlDV,this.dataDV);
+   this.cate.getDatas(this.urlGTh,this.dataGTh);
+   this.cate.getDatas(this.urlTB,this.dataTB);
+  }
 
   ngOnInit(): void {
   }
