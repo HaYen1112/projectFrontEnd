6ba2808941b35950080c1cc5350917a789b2f151
hayen1112 <18130295@st.hcmuaf.edu.vn>
2021-07-20 08:07:27 +0700

    Hoàn thành đổ dữ liệu phần thẻ content Bạn Cần Biết

--- app-news/src/app/components/you-need-know/under-left/under-left.component.ts
+++ app-news/src/app/components/you-need-know/under-left/under-left.component.ts
@@ -6,14 +6,19 @@ import { BodyCenterComponent } from '../../body/page-body/body-center/body-cente
   templateUrl: './under-left.component.html',
   styleUrls: ['./under-left.component.scss']
 })
-export class UnderLeftComponent implements OnInit {
-  urlSlide = " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fgiao-duc%2Ftuyen-sinh%2F2021.rss".trim();
 
+export class UnderLeftComponent implements OnInit {
+  urlSlide = " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Fvideo%2Fthe-gioi.rss&api_key=8rhq1qggacg7vqdw0oxezforxw39rfhvlytwallw".trim();
+  urlnew = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fthanhnien.vn%2Frss%2Fthoi-su%2Fdan-sinh.rss&api_key=8rhq1qggacg7vqdw0oxezforxw39rfhvlytwallw".trim();
   @Input() DataSlide : any[];
+  @Input() Datanew : any[];
 
   constructor(private cate : BodyCenterComponent) {
     this.DataSlide = [];
+    this.Datanew = [];
     this.cate.getDatas(this.urlSlide, this.DataSlide);
+    this.cate.getDatas(this.urlnew, this.Datanew);
+
    }
 
   ngOnInit(): void {
